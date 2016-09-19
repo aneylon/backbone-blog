6cebd78983535d1ad1b56ff1b9590ea41684cdb2
aneylon <arlen.m.neylon@gmail.com>
2016-09-18 19:56:37 -0700

    set up basic routing

--- js/appBb.js
+++ js/appBb.js
@@ -1,4 +1,78 @@
-// backbone things move to other file? or move admin functions to other file?
+var Router = Backbone.Router.extend({
+  routes: {
+    '': 'viewHome',
+    'projects': 'viewProjects',
+    'images': 'viewImages',
+    'about': 'viewAbout',
+    'admin': 'viewAdmin',
+    '*other': 'viewHome'
+  },
+  viewHome: function(){
+    this.loadView(new HomeView({ collection: posts}))
+    // this.loadView(new PostsView({ collection: posts}))
+  },
+  viewProjects: function(){
+    this.loadView(new ProjectsView())
+  },
+  viewImages: function(){
+    this.loadView(new ImagesView())
+  },
+  viewAbout: function(){
+    this.loadView(new AboutView())
+  },
+  loadView: function(view){
+    if(this._currentView){
+      this._currentView.remove()
+    }
+    $('#posts').html(view.render().$el)
+    this._currentView = view
+  }
+})
+
+var HomeView = Backbone.View.extend({
+  render: function(){
+    this.$el.html('Home View')
+    return this
+  }
+})
+
+var ProjectsView = Backbone.View.extend({
+  render: function(){
+    this.$el.html('Projects View')
+    return this
+  }
+})
+
+var ImagesView = Backbone.View.extend({
+  render: function(){
+    this.$el.html('Images View')
+    return this
+  }
+})
+
+var AboutView = Backbone.View.extend({
+  render: function(){
+    this.$el.html('About View')
+    return this
+  }
+})
+
+var router = new Router()
+Backbone.history.start()
+
+var NavView = Backbone.View.extend({
+  events: {
+    'click': 'onClick'
+  },
+  onClick: function(e){
+    console.log('router nav click')
+    var $li = $(e.target)
+    router.navigate($li.attr('data-url'), {trigger: true})
+  }
+})
+
+var navView = new NavView({ el: '#navLinks'})
+
 var Link = Backbone.Model.extend({
   defaults: {
     url: '',
@@ -13,7 +87,8 @@ var Links = Backbone.Collection.extend({
 var LinkView = Backbone.View.extend({
   tagName: 'li',
   // className: 'button grey',
-  template: _.template('<a href="<%= url %>" class="button grey"><%= text %>'),
+  template: _.template('<a data-url="<%= url %>" class="button grey"><%= text %>'),
+  // template: _.template('<a href="<%= url %>" class="button grey"><%= text %>'),
   initilize: function(){
     this.render()
   },
@@ -104,5 +179,5 @@ var PostsView = Backbone.View.extend({
 
 var postsView = new PostsView({ collection: posts })
 setTimeout(function(){
-  postsView.render()
+  // postsView.render()
 },500)
