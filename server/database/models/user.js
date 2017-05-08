c1898bd28ece160d80855a181d7a256d38f8c442
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-05-08 12:46:21 -0600

    fix pw hashing

--- server/database/models/user.js
+++ server/database/models/user.js
@@ -9,17 +9,14 @@ let User = Bookshelf.Model.extend({
     this.on('creating', this.hashPassword)
   },
   comparePassword (password, callback) {
-    // let matches = this.get('password') === password
     bcrypt.compare(password, this.get('password'), function(err, matches){
-      console.log(matches)
       callback(matches)
     })
   },
   hashPassword () {
-    let hashPromise = Bluebird.promisify(bcrypt.hash)
-    return hashPromise(this.get('password'), null, null).bind(this)
-      then(function(hash){
-        console.log(hash)
+    let Hashed = Bluebird.promisify(bcrypt.hash)
+    return Hashed(this.get('password'), null, null).bind(this)
+      .then(function(hash) {
         this.set('password', hash)
       })
   }
