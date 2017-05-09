f66daf50963c380d8bf8033fba3e92ce383eefd9
Arlen Neylon <arlen.m.neylon@gmail.com>
2017-05-08 20:44:15 -0600

    front end auth and view

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ gulp.task('css', () => {
 
 gulp.task('js', () => {
   return gulp.src([
+    './src/js/events.js',
     './src/js/links.js',
     './src/js/about.js',
     './src/js/admin.js',
--- src/js/admin.js
+++ src/js/admin.js
@@ -1,24 +1,88 @@
 /* global
   Backbone
 */
+let SignoutView = Backbone.View.extend({
+  eventBus,
+  template: _.template($('#signoutTemplate').html()),
+  events: {
+    'submit': 'onSubmit'
+  },
+  initialize (options) {
+    if(options.signedIn){
+      this.loggedIn()
+    } else {
+      this.loggedOut()
+    }
+    this.eventBus.on('loggedIn', this.loggedIn, this)
+    this.eventBus.on('loggedOut', this.loggedOut, this)
+  },
+  loggedOut () {
+    this.$el.css({
+      'visibility': 'hidden',
+      'display': 'none'
+    })
+    this.render()
+  },
+  loggedIn () {
+    this.$el.css({
+      'visibility': 'visible',
+      'display': 'initial'
+    })
+    this.render()
+  },
+  onSubmit (e) {
+    e.preventDefault()
+    localStorage.removeItem('bbb-jwt')
+    this.eventBus.trigger('loggedOut')
+  },
+  render () {
+    this.$el.html(this.template)
+    return this
+  }
+})
+
 let SigninView = Backbone.View.extend({
+  eventBus,
   template: _.template($('#signinTemplate').html()),
   events: {
     'submit': 'onSubmit'
   },
+  initialize (options) {
+    if(options.signedIn) {
+      this.loggedIn()
+    } else {
+      this.loggedOut()
+    }
+    this.eventBus.on('loggedIn', this.loggedIn, this)
+    this.eventBus.on('loggedOut', this.loggedOut, this)
+  },
+  loggedOut () {
+    this.$el.css({
+      'visibility': 'visible',
+      'display': 'initial'
+    })
+    this.render()
+  },
+  loggedIn () {
+    this.$el.css({
+      'visibility': 'hidden',
+      'display': 'none'
+    })
+    this.render()
+  },
   onSubmit (e) {
     e.preventDefault()
-    console.log('submit signin')
     let username = $('#signinUsername').val()
     let password = $('#signinPassword').val()
-    console.log({ username, password })
+    let self = this
     $.post(
       '/users/signin',
       { username, password },
       function(data, status) {
-        console.log('data', data)
-        console.log('status', status)
-        // if success store token
+        if(data.success) {
+          localStorage.setItem('bbb-jwt', data.token)
+          self.eventBus.trigger('loggedIn')
+        }
       }
     )
   },
@@ -29,22 +93,19 @@ let SigninView = Backbone.View.extend({
 })
 
 let SignupView = Backbone.View.extend({
+  eventBus,
   template: _.template($('#signupTemplate').html()),
   events: {
     'submit': 'onSubmit'
   },
   onSubmit (e) {
     e.preventDefault()
-    console.log('submit signup')
     let username = $('#signupUsername').val()
     let password = $('#signupPassword').val()
-    console.log({ username, password })
     $.post(
       '/users/signup',
       { username, password },
       function(data, status) {
-        console.log('data', data)
-        console.log('status', status)
       }
     )
   },
@@ -55,23 +116,43 @@ let SignupView = Backbone.View.extend({
 })
 
 let AddPostView = Backbone.View.extend({
+  eventBus,
   template: _.template($('#addPostsTemplate').html()),
   events:{
     'submit': 'onSubmit'
   },
+  initialize (options) {
+    if(options.signedIn) {
+      this.loggedIn()
+    } else {
+      this.loggedOut()
+    }
+    this.eventBus.on('loggedIn', this.loggedIn, this)
+    this.eventBus.on('loggedOut', this.loggedOut, this)
+  },
+  loggedIn () {
+    this.$el.css({
+      'visibility': 'visible',
+      'display': 'initial'
+    })
+    this.render()
+  },
+  loggedOut () {
+    this.$el.css({
+      'visibility': 'hidden',
+      'display': 'none'
+    })
+    this.render()
+  },
   onSubmit (e) {
     e.preventDefault()
-    console.log('add post')
     let title = this.$('#addPostTitle').val()
     let text = this.$('#addPostText').val()
-    console.log({ title, text })
+    let token = localStorage.getItem('bbb-jwt')
     $.post(
       '/posts',
-      // send token
-      { title, text },
+      { title, text, token },
       function(data, status) {
-        console.log('data', data)
-        console.log('status', status)
       }
     )
   },
@@ -83,15 +164,27 @@ let AddPostView = Backbone.View.extend({
 
 let AdminView = Backbone.View.extend({
   el: '#app',
-  template: _.template($('#adminTemplate').html()),
+  eventBus,
+  // template: _.template($('#adminTemplate').html()),
+  initialize () {
+    if(localStorage.getItem('bbb-jwt')){
+      this.eventBus.trigger('loggedIn')
+      this.signedIn = true
+    } else {
+      this.eventBus.trigger('loggedOut')
+      this.signedIn = false
+    }
+  },
   render () {
     // this.$el.html(this.template)
     this.$el.html('')
-    let signinView = new SigninView()
+    let signinView = new SigninView({ signedIn: this.signedIn })
     this.$el.append(signinView.render().$el)
+    let signoutView = new SignoutView({ signedIn: this.signedIn })
+    this.$el.append(signoutView.render().$el)
     let signupView = new SignupView()
     this.$el.append(signupView.render().$el)
-    let addPostView = new AddPostView()
+    let addPostView = new AddPostView({ signedIn: this.signedIn })
     this.$el.append(addPostView.render().$el)
     return this
   }
